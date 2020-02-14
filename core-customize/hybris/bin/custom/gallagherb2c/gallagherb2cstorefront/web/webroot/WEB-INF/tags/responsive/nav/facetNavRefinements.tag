44e3f2f1869e47b2404d6cd23457b186689311b1
Abhishek Kumar <Abhishek.kumar05@nagarro.com>
2020-02-14 18:29:37 +0530

    SAPP-257: check in for facet nav css and script files

--- core-customize/hybris/bin/custom/gallagherb2c/gallagherb2cstorefront/web/webroot/WEB-INF/tags/responsive/nav/facetNavRefinements.tag
+++ core-customize/hybris/bin/custom/gallagherb2c/gallagherb2cstorefront/web/webroot/WEB-INF/tags/responsive/nav/facetNavRefinements.tag
@@ -7,19 +7,47 @@
 
 
 
-<c:forEach items="${pageData.facets}" var="facet" varStatus="i">
-	<c:choose>
-		<c:when test="${facet.code eq 'availableInStores'}">
-			<nav:facetNavRefinementStoresFacet facetData="${facet}" userLocation="${userLocation}"/>
-		</c:when>
-		<c:otherwise>
-		<c:set var="flag" value="" />
-			<c:if test="${i.first}"> 
-			<c:set var="flag" value="show" />
-			</c:if>
-			<nav:facetNavRefinementFacet facetData="${facet}" collapseflag="${flag}"/>
-		</c:otherwise>
-	</c:choose>
-</c:forEach>
+                            <div class="sidebar-filter-section d-flex flex-column justify-content-center">
+                                <div>
+                                    <a href="javascript:void(0)">
+                                        <span class="cross-icon">
+                                            <svg>
+                                                <use xlink:href="${commonResourcePath}/images/gallagher-icons.svg#cross" />
+                                            </svg>
+                                        </span>
+                                        Clear filters
+                                    </a>
+                                </div>
 
+                            </div>
+                
+                
+						<c:forEach items="${pageData.facets}" var="facet" varStatus="i">
+							<c:choose>
+								<c:when test="${facet.code eq 'availableInStores'}">
+									<%-- <nav:facetNavRefinementStoresFacet facetData="${facet}" userLocation="${userLocation}"/> --%>
+								</c:when>
+								<c:when test="${facet.code eq 'price'}">
+									<%-- <nav:facetNavRefinementStoresFacet facetData="${facet}" userLocation="${userLocation}"/> --%>
+									<!-- this part is for price slider   -->
+									<div id="slider-range"></div>
+									<div class="countarea">
+										<input id="my_min" name="" type="text" readonly="readonly" />
+											<strong>to</strong>
+										<input id="my_max" name="" type="text" readonly="readonly" />
+									</div> 
+								</c:when>
+								<c:otherwise>
+								
+								        <c:set var="flag" value="" />
+						            <c:if test="${i.first}"> 
+						            this is 
+						           <c:set var="flag" value="show" />
+						            </c:if>
+									<nav:facetNavRefinementFacet facetData="${facet}" collapseflag="${flag}"/>
+								</c:otherwise>
+							</c:choose>
+						</c:forEach>
 
+					
+	
--- core-customize/hybris/bin/custom/gallagherb2c/gallagherb2cstorefront/web/webroot/_ui/responsive/common/js/custom/acc.productListNav.js
+++ core-customize/hybris/bin/custom/gallagherb2c/gallagherb2cstorefront/web/webroot/_ui/responsive/common/js/custom/acc.productListNav.js
@@ -7,51 +7,50 @@ ACC.productListNav = {
 	productPageNav: function(){
 		$(document).ready(function () {
 			
-			var sliderElement = $('#slider-range');
-			var minValue = $("#my_min");
-			var maxValue = $("#my_max");
-			
-			sliderElement.slider({
+            var sliderElement = $('#slider-range');
+            var minValue = $("#my_min");
+            var maxValue = $("#my_max");
+            
+            sliderElement.slider({
+            
+                  range: true,
+                  min: 0,
+                  max: 5000,
+                  values: [ 0, 100 ],
+                  slide: function( event, ui ) {
+                    // alert()
+                	  minValue.val( "$" + ui.values[ 0 ]);
+                	  maxValue.val( "$" + ui.values[ 1 ] );
+                    /* conssole.log(minValue," : ",maxValue) */
+                  },
+                  stop: function(event, ui){
+                      
+                      if ((typeof minValue.val() !== "undefined") && (typeof maxValue.val() !== "undefined")) {
+                          ajaxCallForPrice(minValue.val(), maxValue.val())
+                          //alert(ACC.config.encodedContextPath +'/Animal-Management/c/Animal%20Management/'+$('#my_min').val()+'/'+$('#my_max').val());
+                      }
+                  }
 		    
-			      range: true,
-			      min: 0,
-			      max: 5000,
-			      values: [ 0, 100 ],
-			      slide: function( event, ui ) {
-			    	// alert()
-			        $( '#my_min' ).val( "$" + ui.values[ 0 ]);
-			        $('#my_max').val( "$" + ui.values[ 1 ] );
-			        /* conssole.log(minValue," : ",maxValue) */
-			      },
-			      stop: function(event, ui){
-			    	  
-			    	  if ((typeof minValue.val() !== "undefined") && (typeof maxValue.val() !== "undefined")) {
-				    	  ajaxCallForPrice(minValue.val(), maxValue.val())
-				    	  //alert(ACC.config.encodedContextPath +'/Animal-Management/c/Animal%20Management/'+$('#my_min').val()+'/'+$('#my_max').val());
-			    	  }
-			      }
-		    });
-		    
-			minValue.val( "$" + sliderElement.slider( "values", 0 ));
-			maxValue.val( "$" + sliderElement.slider( "values", 1 ));
+                  minValue.val( "$" + sliderElement.slider( "values", 0 ));
+                  maxValue.val( "$" + sliderElement.slider( "values", 1 ));
 		});
-		
-		function ajaxCallForPrice(min,max){
-			
-			var pathName = $(location).attr('pathname');
-			console.log(window.location.search);
-			$.ajax({
+            
+        function ajaxCallForPrice(min,max){
+                
+            var pathName = $(location).attr('pathname');
+            console.log(window.location.search);
+            $.ajax({
                 url: pathName,
                 type: 'GET',
                 data: {"price": min+'-'+max},
                 success: function (response) {
-                  // console.log(response)
+                      // console.log(response)
                 },
                 error: function (jqXHR, textStatus, errorThrown) {
-                    
+                      
                 }
             });
-	      }
+        }
 		    
 	}
 }
