4de42e7f3ba70fee39e8b8b4c60283d46c07981b
Vikram Bishnoi <vikram.bishnoi@nagarro.com>
2020-03-08 10:39:00 +0530

    SAPP-201: Redirect to keycloak when cusotmer click on 'Register' button

--- core-customize/hybris/bin/custom/gallagherb2c/gallagherb2cstorefront/web/src/com/gallagher/b2c/controllers/pages/RegisterPageController.java
+++ core-customize/hybris/bin/custom/gallagherb2c/gallagherb2cstorefront/web/src/com/gallagher/b2c/controllers/pages/RegisterPageController.java
@@ -3,6 +3,7 @@
  */
 package com.gallagher.b2c.controllers.pages;
 
+import de.hybris.platform.acceleratorservices.urlresolver.SiteBaseUrlResolutionService;
 import de.hybris.platform.acceleratorstorefrontcommons.controllers.pages.AbstractRegisterPageController;
 import de.hybris.platform.acceleratorstorefrontcommons.forms.RegisterForm;
 import de.hybris.platform.cms2.exceptions.CMSItemNotFoundException;
@@ -14,12 +15,14 @@ import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.gallagher.b2c.controllers.ControllerConstants;
@@ -33,6 +36,14 @@ import com.gallagher.b2c.controllers.ControllerConstants;
 public class RegisterPageController extends AbstractRegisterPageController
 {
 	private HttpSessionRequestCache httpSessionRequestCache;
+
+	@Resource(name = "siteBaseUrlResolutionService")
+	private SiteBaseUrlResolutionService siteBaseUrlResolutionService;
+
+	protected SiteBaseUrlResolutionService getSiteBaseUrlResolutionService()
+	{
+		return siteBaseUrlResolutionService;
+	}
 
 	@Override
 	protected AbstractPageModel getCmsPage() throws CMSItemNotFoundException
@@ -63,13 +74,23 @@ public class RegisterPageController extends AbstractRegisterPageController
 	}
 
 	@RequestMapping(method = RequestMethod.GET)
-	public String doRegister(final Model model) throws CMSItemNotFoundException
+	public String doRegister(final Model model, @RequestParam(value = "code", required = false)
+	final String code) throws CMSItemNotFoundException
 	{
 
 		final String redirectURL = getSiteBaseUrlResolutionService().getWebsiteUrlForSite(getBaseSiteService().getCurrentBaseSite(),
-				true, null);
-		return REDIRECT_PREFIX + MessageFormat
-				.format(getConfigurationService().getConfiguration().getString("keycloak.registrations.url"), redirectURL);
+				true, "/register");
+		String redirectURI = null;
+		if (StringUtils.isEmpty(code))
+		{
+			redirectURI = REDIRECT_PREFIX + MessageFormat
+					.format(getConfigurationService().getConfiguration().getString("keycloak.registrations.url"), redirectURL);
+		}
+		else
+		{
+			redirectURI = REDIRECT_PREFIX + "/login";
+		}
+		return redirectURI;
 	}
 
 	@RequestMapping(value = "/newcustomer", method = RequestMethod.POST)
