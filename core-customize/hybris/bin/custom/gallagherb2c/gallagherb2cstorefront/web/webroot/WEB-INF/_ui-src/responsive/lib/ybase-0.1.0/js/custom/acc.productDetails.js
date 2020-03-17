6990bf017e926f1c3d71f9add72e9ddad380d720
shilpiverma <shilpi.verma@nagarro.com>
2020-03-17 13:18:09 +0530

    SAPP-394 (Implementation of HTML and functionality for Register a Product with modal popup)

--- core-customize/hybris/bin/custom/gallagherb2c/gallagherb2cstorefront/web/webroot/WEB-INF/_ui-src/responsive/lib/ybase-0.1.0/js/custom/acc.productDetails.js
+++ core-customize/hybris/bin/custom/gallagherb2c/gallagherb2cstorefront/web/webroot/WEB-INF/_ui-src/responsive/lib/ybase-0.1.0/js/custom/acc.productDetails.js
@@ -1,9 +1,7 @@
 ACC.productDetails = {
 
 	_autoload: [
-	    "clickOnTab",
-	    "bindSubmitRegisterProduct",
-	    "getJSONDataForRegisterProduct"
+	    "clickOnTab"
 	],
 
 	clickOnTab: function(){
@@ -139,82 +137,5 @@ ACC.productDetails = {
 		                sync: "#productDetailMainCarousel"
 		            });
 		        });
-	},
-	bindSubmitRegisterProduct : function() {
-		$('.registerProduct').click(
-				function() {
-					/*ACC.common.showLoader();*/
-					$.ajax({
-						url : ACC.config.encodedContextPath + '/register/submit',
-						type : 'POST',
-						dataType : 'json',
-						contentType : 'application/json',
-						data : ACC.productDetails.getJSONDataForRegisterProduct(),
-						async : true,
-						success : function(response) {
-							
-							if(response.success != null){
-								alert("1");
-							    /*$('#customerSignup').modal('hide');
-							    ACC.common.hideLoader();
-								$("#customerSignupConfirm").modal('show');
-								$('.return-checkout').click(function() {
-								    location.reload();
-								});*/
-							}
-							/*else{
-									$('#globalMessages span').text(response['globalError']);
-									var errorFields = Object.keys(response);
-									delete response['globalError'];
-									for (var field in errorFields) {
-										if($('#'+errorFields[field]).siblings().hasClass("selecty")){
-											$('#'+errorFields[field]).siblings().find('.selecty-selected').addClass('error');
-										}
-										else{
-											$('#'+errorFields[field]).addClass("error");
-										}
-										$('#'+errorFields[field]).siblings('.errortxt').text(response[errorFields[field]]);
-									}
-									ACC.common.hideLoader();
-								//$('#customerSignup').modal();
-									
-							}*/
-							
-						},
-						error : function(jqXHR, textStatus, errorThrown) {
-							// log the error to the console
-							console.log("The following error occurred: "
-									+ textStatus, errorThrown);
-							/*ACC.common.handleAccessDeniedError(jqXHR.status);*/
-						}
-					});
-				});
-
-	},
-	getJSONDataForRegisterProduct : function() {
-		/*ACC.common.resetSignUpForm();*/
-		var productSku=$("#productSku").val();
-		var serialNumber = $("#serialNumber").val();
-		var datePurchased = $("#datePurchased").val();
-		var addressLine1 = $("#addressLine1").val();
-		var addressLine2 = $("#addressLine2").val();
-		var townCity = $("#townCity").val();
-		var postCode = $("#postCode").val();
-		var country = $("#country").val();
-		var phoneNumber = $("#phoneNumber").val();
-		
-		var productDetails = {
-				"productSku" : productSku,
-				"serialNumber" : serialNumber,
-				"datePurchased" : datePurchased,
-				"addressLine1" : addressLine1,
-				"addressLine2" : addressLine2,
-				"townCity" : townCity,
-				"postCode" : postCode,
-				"country" : country,
-				"phoneNumber" : phoneNumber
-		}
-		
-		return JSON.stringify(productDetails);
 	}
 };
