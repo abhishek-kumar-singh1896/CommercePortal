1133ecd80fa3ffa7d421de7ee051020a41504bdd
Gaurav Kamboj <gauravkambojtu@gmail.com>
2020-03-29 12:29:42 +0530

    SAPP-67: push newly rgistered B2B users from backoffice to C4C

---
+++ core-customize/hybris/bin/custom/gallagher/gallaghercore/src/com/gallagher/core/daos/GallagherCurrencyDao.java
@@ -0,0 +1,22 @@
+/**
+ *
+ */
+package com.gallagher.core.daos;
+
+import de.hybris.platform.core.model.c2l.CurrencyModel;
+
+
+/**
+ * DAO for methods related to CurrencyModel
+ *
+ * @author gauravkamboj
+ */
+public interface GallagherCurrencyDao
+{
+
+	/**
+	 * get CurrencyModel by ISO Code
+	 *
+	 */
+	CurrencyModel getCurrencyByIsoCode(String isoCode);
+}
