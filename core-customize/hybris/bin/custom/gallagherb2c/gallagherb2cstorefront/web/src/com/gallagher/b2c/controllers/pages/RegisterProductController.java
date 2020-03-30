141f6c7f750f8ec76b84516ac31e8a83282e244a
Gaurav Kamboj <gauravkambojtu@gmail.com>
2020-03-30 15:03:14 +0530

    SAPP-339: image added for registered products page | hardcoded data as of now

--- core-customize/hybris/bin/custom/gallagherb2c/gallagherb2cstorefront/web/src/com/gallagher/b2c/controllers/pages/RegisterProductController.java
+++ core-customize/hybris/bin/custom/gallagherb2c/gallagherb2cstorefront/web/src/com/gallagher/b2c/controllers/pages/RegisterProductController.java
@@ -15,6 +15,7 @@ import de.hybris.platform.commercefacades.product.ProductFacade;
 import de.hybris.platform.commercefacades.product.ProductOption;
 import de.hybris.platform.commercefacades.product.data.ImageData;
 import de.hybris.platform.commercefacades.product.data.ProductData;
+import de.hybris.platform.product.impl.DefaultProductService;
 import de.hybris.platform.servicelayer.exceptions.UnknownIdentifierException;
 
 import java.util.Arrays;
@@ -77,6 +78,9 @@ public class RegisterProductController extends AbstractPageController
 	@Resource(name = "contentPageBreadcrumbBuilder")
 	private ContentPageBreadcrumbBuilder contentPageBreadcrumbBuilder;
 
+	@Resource(name = "productService")
+	private DefaultProductService defaultProductService;
+
 	@RequestMapping(value = "/products", method = RequestMethod.GET)
 	public String registeredProductsByUser(final Model model) throws CMSItemNotFoundException
 	{
@@ -85,6 +89,12 @@ public class RegisterProductController extends AbstractPageController
 		setUpMetaDataForContentPage(model, regProductsPage);
 		model.addAttribute("registeredProducts", gallagherRegisteredProductsFacade.getRegisteredProducts());
 		model.addAttribute(WebConstants.BREADCRUMBS_KEY, contentPageBreadcrumbBuilder.getBreadcrumbs(regProductsPage));
+		//below attribute is for mocking purpose only. will be removed after GET call from C4C is used.
+		if (defaultProductService.getProductForCode("solar-fence-energizer-s10") != null)
+		{
+		model.addAttribute("imageUrl",
+				defaultProductService.getProductForCode("solar-fence-energizer-s10").getGalleryImages().get(0).getMaster().getURL());
+		}
 		return getViewForPage(model);
 	}
 
