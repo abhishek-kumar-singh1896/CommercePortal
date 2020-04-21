68ad608d234ef7e72706a7f613491e4170653f11
Vikram Bishnoi <vikram.bishnoi@nagarro.com>
2020-04-21 12:09:09 +0530

    SAPP-471 : Sharing cart cookie with sitecore

--- core-customize/hybris/bin/custom/gallagherb2c/gallagherb2cstorefront/web/src/com/gallagher/b2c/security/cookie/CartRestoreCookieGenerator.java
+++ core-customize/hybris/bin/custom/gallagherb2c/gallagherb2cstorefront/web/src/com/gallagher/b2c/security/cookie/CartRestoreCookieGenerator.java
@@ -5,12 +5,19 @@ package com.gallagher.b2c.security.cookie;
 
 import de.hybris.platform.site.BaseSiteService;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletResponseWrapper;
+
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Required;
 
+import com.google.common.net.InternetDomainName;
+
 
 /**
- * 
+ *
  */
 public class CartRestoreCookieGenerator extends EnhancedCookieGenerator
 {
@@ -31,5 +38,33 @@ public class CartRestoreCookieGenerator extends EnhancedCookieGenerator
 	public void setBaseSiteService(final BaseSiteService baseSiteService)
 	{
 		this.baseSiteService = baseSiteService;
+	}
+
+
+	public void addCookie(final HttpServletRequest request, final HttpServletResponse response, final String cookieValue)
+	{
+		super.addCookie(new HttpServletResponseWrapper(response) // NOSONAR
+		{
+			@Override
+			public void addCookie(final Cookie cookie)
+			{
+				setEnhancedCookiePath(cookie);
+
+				final InternetDomainName domainName = InternetDomainName.from(request.getServerName());
+				if (domainName.isTopPrivateDomain() || domainName.hasPublicSuffix())
+				{
+					final String url = domainName.topPrivateDomain().toString();
+					final InternetDomainName it = InternetDomainName.from(url);
+					cookie.setDomain(it.toString());
+				}
+
+				if (isHttpOnly())
+				{
+					cookie.setHttpOnly(true);
+				}
+
+				super.addCookie(cookie);
+			}
+		}, cookieValue);
 	}
 }
