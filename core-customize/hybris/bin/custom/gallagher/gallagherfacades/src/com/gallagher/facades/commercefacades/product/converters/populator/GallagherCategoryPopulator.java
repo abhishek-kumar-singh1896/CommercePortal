0abe1818b89515d598fc7b962b149167fe4e1529
shilpiverma <shilpi.verma@nagarro.com>
2020-05-14 17:29:13 +0530

    SAPP-549 : The navigation of product categories is incorrect

---
+++ core-customize/hybris/bin/custom/gallagher/gallagherfacades/src/com/gallagher/facades/commercefacades/product/converters/populator/GallagherCategoryPopulator.java
@@ -0,0 +1,25 @@
+/**
+ *
+ */
+package com.gallagher.facades.commercefacades.product.converters.populator;
+
+import de.hybris.platform.category.model.CategoryModel;
+import de.hybris.platform.commercefacades.product.converters.populator.CategoryPopulator;
+import de.hybris.platform.commercefacades.product.data.CategoryData;
+
+
+/**
+ * populating description from category model.
+ *
+ * @author shilpiverma
+ */
+public class GallagherCategoryPopulator extends CategoryPopulator
+{
+	@Override
+	public void populate(final CategoryModel source, final CategoryData target)
+	{
+		super.populate(source, target);
+		target.setDescription(source.getDescription());
+	}
+
+}
