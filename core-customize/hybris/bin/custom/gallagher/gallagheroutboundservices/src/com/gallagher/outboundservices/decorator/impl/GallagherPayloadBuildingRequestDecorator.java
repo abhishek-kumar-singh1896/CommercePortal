c8e404a0efa505d1ee40a110374ad102b6754d18
Vikram Bishnoi <vikram.bishnoi@nagarro.com>
2020-05-20 18:16:42 +0530

    SAPP-547 : Payload Changes for communication preferences

--- core-customize/hybris/bin/custom/gallagher/gallagheroutboundservices/src/com/gallagher/outboundservices/decorator/impl/GallagherPayloadBuildingRequestDecorator.java
+++ core-customize/hybris/bin/custom/gallagher/gallagheroutboundservices/src/com/gallagher/outboundservices/decorator/impl/GallagherPayloadBuildingRequestDecorator.java
@@ -1,9 +1,16 @@
 package com.gallagher.outboundservices.decorator.impl;
 
 import de.hybris.platform.core.model.user.CustomerModel;
+import de.hybris.platform.integrationservices.model.IntegrationObjectDescriptor;
+import de.hybris.platform.integrationservices.model.IntegrationObjectModel;
+import de.hybris.platform.integrationservices.model.TypeDescriptor;
+import de.hybris.platform.integrationservices.model.impl.DefaultIntegrationObjectDescriptor;
+import de.hybris.platform.integrationservices.service.IntegrationObjectNotFoundException;
+import de.hybris.platform.integrationservices.service.IntegrationObjectService;
 import de.hybris.platform.outboundservices.decorator.DecoratorContext;
 import de.hybris.platform.outboundservices.decorator.DecoratorExecution;
 import de.hybris.platform.outboundservices.decorator.impl.DefaultPayloadBuildingRequestDecorator;
+import de.hybris.platform.servicelayer.exceptions.ModelNotFoundException;
 
 import java.util.Map;
 
@@ -14,6 +21,7 @@ import org.springframework.http.HttpHeaders;
 public class GallagherPayloadBuildingRequestDecorator extends DefaultPayloadBuildingRequestDecorator
 {
 
+	private IntegrationObjectService integrationObjectService;
 
 	@Override
 	public HttpEntity<Map<String, Object>> decorate(final HttpHeaders httpHeaders, final Map<String, Object> payload,
@@ -22,26 +30,30 @@ public class GallagherPayloadBuildingRequestDecorator extends DefaultPayloadBuil
 		final Map<String, Object> map = getIntegrationObjectConversionService().convert(context.getItemModel(),
 				context.getIntegrationObjectCode());
 
+
 		if (context.getItemModel() instanceof CustomerModel)
 		{
-			final boolean value = map.containsKey("newsLetters");
-			if (map.get("newsLetters") == null)
+
+			final TypeDescriptor descriptor = findIntegrationObjectDescriptor(context.getIntegrationObjectCode())
+					.getItemTypeDescriptor(context.getItemModel()).get();
+
+			if (map.get("newsLetters") == null && descriptor.getAttribute("newsLetters").isPresent())
 			{
 				map.put("newsLetters", false);
 			}
-			if (map.get("productPromo") == null)
+			if (map.get("productPromo") == null && descriptor.getAttribute("productPromo").isPresent())
 			{
 				map.put("productPromo", false);
 			}
-			if (map.get("events") == null)
+			if (map.get("events") == null && descriptor.getAttribute("events").isPresent())
 			{
 				map.put("events", false);
 			}
-			if (map.get("productRelease") == null)
+			if (map.get("productRelease") == null && descriptor.getAttribute("productRelease").isPresent())
 			{
 				map.put("productRelease", false);
 			}
-			if (map.get("productUpdate") == null)
+			if (map.get("productUpdate") == null && descriptor.getAttribute("productUpdate").isPresent())
 			{
 				map.put("productUpdate", false);
 			}
@@ -51,5 +63,26 @@ public class GallagherPayloadBuildingRequestDecorator extends DefaultPayloadBuil
 		return execution.createHttpEntity(httpHeaders, payload, context);
 	}
 
+	protected IntegrationObjectDescriptor findIntegrationObjectDescriptor(final String integrationObjectCode)
+	{
+		try
+		{
+			final IntegrationObjectModel model = integrationObjectService.findIntegrationObject(integrationObjectCode);
+			return DefaultIntegrationObjectDescriptor.create(model);
+		}
+		catch (final ModelNotFoundException e)
+		{
+			throw new IntegrationObjectNotFoundException(integrationObjectCode, e);
+		}
+	}
 
+	protected IntegrationObjectService getIntegrationObjectService()
+	{
+		return integrationObjectService;
+	}
+
+	public void setIntegrationObjectService(final IntegrationObjectService integrationObjectService)
+	{
+		this.integrationObjectService = integrationObjectService;
+	}
 }
--- core-customize/hybris/bin/custom/gallagher/gallagherscpiexchange/src/com/gallagher/sapcustomerb2c/outbound/GallagherSAPCustomerInterceptor.java
+++ core-customize/hybris/bin/custom/gallagher/gallagherscpiexchange/src/com/gallagher/sapcustomerb2c/outbound/GallagherSAPCustomerInterceptor.java
@@ -5,30 +5,29 @@ package com.gallagher.sapcustomerb2c.outbound;
 
 import de.hybris.platform.core.model.user.CustomerModel;
 
+import java.util.HashSet;
 import java.util.Set;
 
 import com.sap.hybris.sapcustomerb2c.outbound.DefaultCustomerInterceptor;
 
 
 /**
+ * Custom interceptor to push customer update for communication preferences
  *
- *
+ * @author Vikram Bishnoi
  */
-public class GallagherCustomerInterceptor extends DefaultCustomerInterceptor
+public class GallagherSAPCustomerInterceptor extends DefaultCustomerInterceptor
 {
 
 	@Override
 	protected Set<String> getMonitoredAttributes()
 	{
-
-		final Set<String> monitoredAttributes = super.getMonitoredAttributes();
+		final Set<String> monitoredAttributes = new HashSet<>();
 		monitoredAttributes.add(CustomerModel.NEWSLETTERS);
 		monitoredAttributes.add(CustomerModel.EVENTS);
 		monitoredAttributes.add(CustomerModel.PRODUCTRELEASE);
 		monitoredAttributes.add(CustomerModel.PRODUCTUPDATE);
 		monitoredAttributes.add(CustomerModel.PRODUCTPROMO);
-
 		return monitoredAttributes;
-
 	}
 }
