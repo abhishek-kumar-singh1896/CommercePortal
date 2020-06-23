ce4b14d9e0a40ea9bbd1e36059d326dce54feb99
Vikram Bishnoi <vikram.bishnoi@nagarro.com>
2020-06-23 20:39:14 +0530

    SAPP-697 : Show all valid languages in SmartEdit

--- core-customize/hybris/bin/custom/gallagher/gallagherfacades/src/com/gallagher/facades/storesession/impl/GallagherStoreSessionFacadeImpl.java
+++ core-customize/hybris/bin/custom/gallagher/gallagherfacades/src/com/gallagher/facades/storesession/impl/GallagherStoreSessionFacadeImpl.java
@@ -1,8 +1,23 @@
 package com.gallagher.facades.storesession.impl;
 
+import de.hybris.platform.basecommerce.model.site.BaseSiteModel;
+import de.hybris.platform.cms2.model.site.CMSSiteModel;
+import de.hybris.platform.commercefacades.storesession.data.LanguageData;
 import de.hybris.platform.commercefacades.storesession.impl.DefaultStoreSessionFacade;
+import de.hybris.platform.converters.Converters;
+import de.hybris.platform.core.model.c2l.LanguageModel;
+import de.hybris.platform.core.model.user.EmployeeModel;
+import de.hybris.platform.servicelayer.user.UserService;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.apache.commons.collections4.CollectionUtils;
 import org.apache.log4j.Logger;
+import org.springframework.util.Assert;
 
 import com.gallagher.facades.storesession.GallagherStoreSessionFacade;
 
@@ -16,6 +31,9 @@ public class GallagherStoreSessionFacadeImpl extends DefaultStoreSessionFacade i
 {
 	private static final Logger LOG = Logger.getLogger(GallagherStoreSessionFacadeImpl.class);
 
+	@Resource
+	private UserService userService;
+
 	/**
 	 * {@inheritDoc}
 	 */
@@ -25,4 +43,49 @@ public class GallagherStoreSessionFacadeImpl extends DefaultStoreSessionFacade i
 		getSessionService().removeAttribute("encodingAttributes");
 
 	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public Collection<LanguageData> getAllLanguages()
+	{
+		Collection<LanguageModel> languages = null;
+		/* This is for the smartedit */
+		if (userService.getCurrentUser().getClass() == EmployeeModel.class && getBaseStoreService().getCurrentBaseStore() != null)
+		{
+			for (final BaseSiteModel site : getBaseStoreService().getCurrentBaseStore().getCmsSites())
+			{
+				final Collection<LanguageModel> catalogLanguages = new ArrayList<>(
+						((CMSSiteModel) site).getContentCatalogs().get(0).getLanguages());
+				if (CollectionUtils.isNotEmpty(catalogLanguages))
+				{
+					final List<LanguageModel> fallbacks = new ArrayList<>(catalogLanguages.size());
+					catalogLanguages.forEach(language -> {
+						if (CollectionUtils.isNotEmpty(language.getFallbackLanguages())
+								&& (!fallbacks.contains(language.getFallbackLanguages().get(0)))
+								&& (!catalogLanguages.contains(language.getFallbackLanguages().get(0))))
+						{
+							fallbacks.add(language.getFallbackLanguages().get(0));
+						}
+					});
+					languages = new ArrayList<>(catalogLanguages);
+					languages.addAll(fallbacks);
+					break;
+				}
+			}
+		}
+		else
+		{
+			languages = getCommerceCommonI18NService().getAllLanguages();
+		}
+		if (languages.isEmpty())
+		{
+			languages = getCommonI18NService().getAllLanguages();
+		}
+		Assert.notEmpty(languages, "No supported languages found for the current site.");
+
+		return Converters.convertAll(languages, getLanguageConverter());
+	}
+
 }
