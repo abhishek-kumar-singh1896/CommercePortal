97399614bbced62c6c6d2f8d7ed1392500db3e05
Gaurav Kamboj <gauravkambojtu@gmail.com>
2020-09-24 17:25:30 +0530

    SAPP-724: facetDisplayNameProvider for adding units

---
+++ core-customize/hybris/bin/custom/gallagher/gallaghercore/src/com/gallagher/core/solrfacetsearch/solr/impl/GallagherFacetUnitSuffixDisplayNameProvider.java
@@ -0,0 +1,66 @@
+/**
+ *
+ */
+package com.gallagher.core.solrfacetsearch.solr.impl;
+
+import de.hybris.platform.solrfacetsearch.config.IndexedProperty;
+import de.hybris.platform.solrfacetsearch.provider.FacetDisplayNameProvider;
+import de.hybris.platform.solrfacetsearch.provider.FacetValueDisplayNameProvider;
+import de.hybris.platform.solrfacetsearch.search.SearchQuery;
+
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+
+
+/**
+ * Facet display name provider to add unit to the value
+ *
+ * @author gauravkamboj
+ */
+public class GallagherFacetUnitSuffixDisplayNameProvider implements FacetDisplayNameProvider, FacetValueDisplayNameProvider
+{
+
+	private final Map<String, Locale> localeCache = new HashMap<>();
+
+	@Override
+	public String getDisplayName(final SearchQuery query, final IndexedProperty property, final String facetValue)
+	{
+		final Locale locale = getLocale(query.getLanguage());
+		final String unit = property.getClassAttributeAssignment().getUnit().getName(locale);
+		return facetValue + " " + unit;
+	}
+
+	/**
+	 * @deprecated Since 5.4.
+	 */
+	@Deprecated(since = "5.4")
+	@Override
+	public String getDisplayName(final SearchQuery var1, final String var2)
+	{
+		// XXX Auto-generated method stub
+		return null;
+	}
+
+	protected Locale getLocale(final String isoCode)
+	{
+		Locale result;
+		result = localeCache.get(isoCode);
+		if (result == null)
+		{
+			final String[] splitted_code = isoCode.split("_");
+			if (splitted_code.length == 1)
+			{
+				result = new Locale(splitted_code[0]);
+			}
+			else
+			{
+				result = new Locale(splitted_code[0], splitted_code[1]);
+			}
+
+			localeCache.put(isoCode, result);
+		}
+		return result;
+	}
+
+}
