509313739914292b65f08fce1b0479db642c923e
shilpiverma <shilpi.verma@nagarro.com>
2020-10-01 12:53:17 +0530

    SAPP2-126 :Tax's are calculated incorrectly on discounted orders

--- core-customize/hybris/bin/custom/gallagher/gallaghercore/src/com/gallagher/core/util/GallagherSovosUtil.java
+++ core-customize/hybris/bin/custom/gallagher/gallaghercore/src/com/gallagher/core/util/GallagherSovosUtil.java
@@ -8,12 +8,15 @@ import de.hybris.platform.core.model.order.AbstractOrderModel;
 import de.hybris.platform.core.model.user.AddressModel;
 import de.hybris.platform.ordersplitting.model.WarehouseModel;
 import de.hybris.platform.storelocator.model.PointOfServiceModel;
+import de.hybris.platform.util.DiscountValue;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.commons.collections4.CollectionUtils;
 
@@ -49,6 +52,24 @@ public class GallagherSovosUtil
 		request.setTrnSrc(abstractOrder.getCode());
 		request.setDlvrAmt(abstractOrder.getDeliveryCost());
 
+		final List<DiscountValue> globalDiscounts = abstractOrder.getGlobalDiscountValues();
+		final Map<String, Double> globalMap = new HashMap<String, Double>();
+		if (globalDiscounts.size() > 0)
+		{
+			double discounts = 0;
+			for (final DiscountValue value : globalDiscounts)
+			{
+				discounts += value.getAppliedValue();
+			}
+			globalMap.put("1", discounts);
+			request.setDiscnts(globalMap);
+		}
+		else
+		{
+			globalMap.put("1", 0.0);
+			request.setDiscnts(globalMap);
+		}
+
 		final List<GallagherSovosCalculateTaxLineItem> lineItems = new ArrayList<>();
 
 		for (final AbstractOrderEntryModel orderEntry : abstractOrder.getEntries())
@@ -64,6 +85,23 @@ public class GallagherSovosUtil
 			lineItem.setTrnTp(sovosConfiguration.getTransactionType());
 			lineItem.setOrgCd(sovosConfiguration.getOrganizationCode());
 			lineItem.setDropShipInd(sovosConfiguration.getDropShipInd());
+			final List<DiscountValue> lineDiscounts = orderEntry.getDiscountValues();
+			final Map<String, Double> lineMap = new HashMap<String, Double>();
+			if (lineDiscounts.size() > 0)
+			{
+				double discounts = 0;
+				for (final DiscountValue discnt : lineDiscounts)
+				{
+					discounts += discnt.getAppliedValue();
+				}
+				lineMap.put("1", discounts);
+				lineItem.setDiscnts(lineMap);
+			}
+			else
+			{
+				lineMap.put("1", 0.0);
+				lineItem.setDiscnts(lineMap);
+			}
 
 			final List<WarehouseModel> warehouses = abstractOrder.getStore().getWarehouses();
 			if (CollectionUtils.isNotEmpty(warehouses))
