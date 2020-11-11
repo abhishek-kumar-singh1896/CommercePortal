b3743ff729c81cb50f8143f03c1f7dd81b231b0a
shilpiverma <shilpi.verma@nagarro.com>
2020-11-11 12:37:25 +0530

    SAPP2-145 : Merging issue resolved

--- core-customize/hybris/bin/custom/gallagher/gallagherfacades/src/com/gallagher/facades/order/converters/populator/GallagherB2BCartPopulator.java
+++ core-customize/hybris/bin/custom/gallagher/gallagherfacades/src/com/gallagher/facades/order/converters/populator/GallagherB2BCartPopulator.java
@@ -14,6 +14,7 @@ import de.hybris.platform.servicelayer.dto.converter.ConversionException;
 import de.hybris.platform.servicelayer.dto.converter.Converter;
 
 import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.util.Assert;
 
 
