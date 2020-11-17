704d90d08f8d0114ed8f5850ca3545d9e1743130
satvir_ecommbi <satvir.singh@ecommbi.com>
2020-11-17 17:41:20 +1300

    - sapsubscriptionaddon and SAP integration SCPI objects

---
+++ core-customize/hybris/bin/custom/playpenbackoffice/backoffice/src/org/training/services/PlaypenbackofficeService.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2019 SAP SE or an SAP affiliate company. All rights reserved
+ */
+package org.training.services;
+
+/**
+ * Hello World PlaypenbackofficeService
+ */
+public class PlaypenbackofficeService
+{
+	public String getHello()
+	{
+		return "Hello";
+	}
+}
