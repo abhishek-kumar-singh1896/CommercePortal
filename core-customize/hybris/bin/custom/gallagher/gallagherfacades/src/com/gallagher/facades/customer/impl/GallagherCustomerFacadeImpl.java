a1c266516fb5e0dd00d8362e3d7f902ce450eca6
Ankit Uniyal <ankit.uniyal@nagarro.com>
2020-11-26 16:12:04 +0530

    Added business unit for guest checkout

--- core-customize/hybris/bin/custom/gallagher/gallagherfacades/src/com/gallagher/facades/customer/impl/GallagherCustomerFacadeImpl.java
+++ core-customize/hybris/bin/custom/gallagher/gallagherfacades/src/com/gallagher/facades/customer/impl/GallagherCustomerFacadeImpl.java
@@ -3,13 +3,18 @@
  */
 package com.gallagher.facades.customer.impl;
 
+import static de.hybris.platform.servicelayer.util.ServicesUtil.validateParameterNotNullStandardMessage;
+
 import de.hybris.platform.commercefacades.customer.impl.DefaultCustomerFacade;
 import de.hybris.platform.commerceservices.customer.DuplicateUidException;
+import de.hybris.platform.commerceservices.enums.CustomerType;
 import de.hybris.platform.commerceservices.enums.SiteChannel;
+import de.hybris.platform.core.model.user.CustomerModel;
 
 import javax.annotation.Resource;
 
 import com.gallagher.core.dtos.GallagherAccessToken;
+import com.gallagher.core.enums.BU;
 import com.gallagher.core.services.GallagherCustomerService;
 import com.gallagher.facades.customer.GallagherCustomerFacade;
 
@@ -48,6 +53,26 @@ public class GallagherCustomerFacadeImpl extends DefaultCustomerFacade implement
 		getGallagherCustomerService().changeUid(newUid);
 
 
+	}
+
+
+	@Override
+	public void createGuestUserForAnonymousCheckout(final String email, final String name) throws DuplicateUidException
+	{
+		validateParameterNotNullStandardMessage("email", email);
+		final CustomerModel guestCustomer = getModelService().create(CustomerModel.class);
+		final String guid = generateGUID();
+
+		//takes care of localizing the name based on the site language
+		guestCustomer.setUid(guid + "|" + email);
+		guestCustomer.setName(name);
+		guestCustomer.setType(CustomerType.valueOf(CustomerType.GUEST.getCode()));
+		guestCustomer.setSessionLanguage(getCommonI18NService().getCurrentLanguage());
+		guestCustomer.setSessionCurrency(getCommonI18NService().getCurrentCurrency());
+		guestCustomer.setBusinessUnit(BU.AM);
+
+		getCustomerAccountService().registerGuestForAnonymousCheckout(guestCustomer, guid);
+		updateCartWithGuestForAnonymousCheckout(getCustomerConverter().convert(guestCustomer));
 	}
 
 
