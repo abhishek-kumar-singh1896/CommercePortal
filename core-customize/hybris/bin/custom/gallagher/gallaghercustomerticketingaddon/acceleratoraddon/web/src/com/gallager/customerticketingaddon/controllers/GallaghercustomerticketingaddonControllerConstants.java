da521bdd8ecc01b22b4b7ae78d1ff80df950786a
Ishaan Vashishth <ishaan.vashishth@nagarro.com>
2021-01-04 11:08:57 +0530

    Gallagher Customer Ticketing Addon Extesntion

---
+++ core-customize/hybris/bin/custom/gallagher/gallaghercustomerticketingaddon/acceleratoraddon/web/src/com/gallager/customerticketingaddon/controllers/GallaghercustomerticketingaddonControllerConstants.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2019 SAP SE or an SAP affiliate company. All rights reserved.
+ */
+package com.gallager.customerticketingaddon.controllers;
+
+/**
+ */
+public interface GallaghercustomerticketingaddonControllerConstants
+{
+	String ADDON_PREFIX = "addon:/gallaghercustomerticketingaddon/";
+}
